fac9848bad7a2dc099d0857fd547dc7f4fb969e6
xiaobiao <343003079@qq.com>
2020-07-01 11:30:58 +0800

    测试

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int biao=0;
+        int biao = 0;
         int a = 2;
+        int b = 0;
         System.out.println("Hello World!");
     }
 }
