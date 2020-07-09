db2eab5c3c2ef0ac1a30cd91cda1e20d49fac716
xiaobiao <343003079@qq.com>
2020-07-09 23:45:41 +0800

    测试

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> arrs = Arrays.asList("123", "3456", "12345", "12", "654", "54646");
+        ArrayList<String> arrs =  new ArrayList<>();
+        arrs.add("123");
+        arrs.add("3456");
+        arrs.add("12345");
+        arrs.add("12");
+        arrs.add("546465");
+        arrs.add("654");
+        arrs.add("54646");
         for (int i = 0; i < arrs.size(); ++i) {
             for (int j = arrs.size() - 1; j > i; --j) {
                 if (arrs.get(i).contains(arrs.get(j))) {
-                    arrs.remove(arrs.get(j));
+                    arrs.remove(j);
 
                 } else if (arrs.get(j).contains(arrs.get(i))) {
                     arrs.remove(arrs.get(i));
