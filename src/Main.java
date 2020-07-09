112eb8b18f5d05fffff10d6f0c0fe609eb712ecd
xiaobiao <343003079@qq.com>
2020-07-09 23:24:20 +0800

    测试

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        int biao = 0;
-        int a = 2;
-        int b = 0;
-        System.out.println("Hello World!");
+        List<String> arrs = Arrays.asList("123", "3456", "12345", "12", "654", "54646");
+        for (int i = 0; i < arrs.size(); ++i) {
+            for (int j = arrs.size() - 1; j > i; --j) {
+                if (arrs.get(i).contains(arrs.get(j))) {
+                    arrs.remove(arrs.get(j));
+
+                } else if (arrs.get(j).contains(arrs.get(i))) {
+                    arrs.remove(arrs.get(i));
+                }
+            }
+        }
+
+        for (String arr : arrs) {
+            System.out.println(arr);
+        }
     }
 }
