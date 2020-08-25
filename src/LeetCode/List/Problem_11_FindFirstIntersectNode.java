beed79ce35877c88ccb21fbb095e3e0c279df02c
xiaobiao <343003079@qq.com>
2020-08-26 07:38:24 +0800

    8.25晚上两道题

--- src/LeetCode/List/Problem_11_FindFirstIntersectNode.java
+++ src/LeetCode/List/Problem_11_FindFirstIntersectNode.java
@@ -118,35 +118,113 @@ public class Problem_11_FindFirstIntersectNode {
     /**
      * 判断两个无环链表是否相交
      * 方法一：
-     *      1：遍历两个链表，看尾节点是否相同，相同说明相交；
-     *      2：遍历的时候入栈，从最后节点开始比较是否相同
+     * 1：遍历两个链表，看尾节点是否相同，相同说明相交；
+     * 2：遍历的时候入栈，从最后节点开始比较是否相同
      * 方法二：
-     *      1：遍历两个链表，记录链表长度，看尾节点是否相同，相同说明相交；
-     *      2：长的链表先走一段，之后在一起走，什么时候相同，则是相交点
+     * 1：遍历两个链表，记录链表长度，看尾节点是否相同，相同说明相交；
+     * 2：长的链表先走一段，之后在一起走，什么时候相同，则是相交点
      *
      * @param head1
      * @param head2
      * @return
      */
-    public static Node noLoop(Node head1,Node head2){
-        if(head1 ==null||head2==null){
+    public static Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
             return null;
         }
         Node cur1 = head1;
         Node cur2 = head2;
         int n = 0;
-        while (cur1.next!=null){
+        while (cur1.next != null) {
             n++;
             cur1 = cur1.next;
         }
-        while (cur2.next!=null){
+        while (cur2.next != null) {
             n--;
             cur2 = cur2.next;
         }
-        if(cur1!=cur2){
+        if (cur1 != cur2) {
+            return null;
+        }
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    /**
+     * 判断两个有环链表是否相交，以及相交节点
+     * 两个链表不相交
+     * 相交又分两种情况，入环节点相同，找到一个；入环节点不相同，则分别返回一个入环节点
+     *
+     * @param head1
+     * @param loop1
+     * @param head2
+     * @param loop2
+     * @return
+     */
+    public static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2) {
+        Node cur1 = null;
+        Node cur2 = null;
+        if (loop1 == loop2) {
+            //有相同的入环节点
+            cur1 = head1;
+            cur2 = head2;
+            int n = 0;
+            while (cur1 != loop1) {
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2 != loop2) {
+                n--;
+                cur2 = cur2.next;
+            }
+            cur1 = n > 0 ? head1 : head2;
+            cur2 = cur1 == head1 ? head2 : head1;
+            n = Math.abs(n);
+            while (n != 0) {
+                n--;
+                cur1 = cur1.next;
+            }
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 == loop2) {
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
             return null;
         }
 
+    }
+
+    public static Node getIntersectNode(Node head1, Node head2) {
+        if(head1==null ||head2==null){
+            return null;
+        }
+        Node loop1 = getLoopNode(head1);
+        Node loop2 = getLoopNode(head2);
+        if (loop1 == null && loop2 == null) {
+            return noLoop(head1,head2);
+        }
+        if(loop1 !=null&& loop2!=null){
+            return bothLoop(head1,loop1,head2,loop2);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
@@ -165,6 +243,13 @@ public class Problem_11_FindFirstIntersectNode {
         System.out.println("链表环的长度：" + circleLen);
         int listLen = getListLen(head1);
         System.out.println("链表的长度：" + listLen);
+
+        // 0->9->8->2...
+        Node head2 = new Node(0);
+        head2.next = new Node(9);
+        head2.next.next = new Node(8);
+        head2.next.next.next = head1.next; // 8->2
+        System.out.println(getIntersectNode(head1, head2).value);
 
 
     }
