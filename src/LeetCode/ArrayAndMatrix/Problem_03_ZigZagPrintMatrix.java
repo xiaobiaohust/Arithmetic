10eeb212d3a11556784b5eee0ac7b0c5a1ad3b53
xiaobiao <343003079@qq.com>
2020-09-15 18:53:17 +0800

    9.15 2个

--- src/LeetCode/ArrayAndMatrix/Problem_03_ZigZagPrintMatrix.java
+++ src/LeetCode/ArrayAndMatrix/Problem_03_ZigZagPrintMatrix.java
@@ -6,19 +6,40 @@ package LeetCode.ArrayAndMatrix;
  * 思路，斜着打印矩阵，上下坐标同时移动
  */
 public class Problem_03_ZigZagPrintMatrix {
-    public static void printMatrixZigZag(int[][]matrix){
+    public static void printMatrixZigZag(int[][] matrix) {
         int tr = 0;
         int tc = 0;
         int dr = 0;
-        int dc= 0;
-        int endr = matrix.length-1;
-        int endc= matrix[0].length-1;
-        boolean fromUp = false;
-        while (tr<endr+1){
-            printLevel(matrix,tr,tc,dr,dc,fromUp);
-
+        int dc = 0;
+        int endr = matrix.length - 1;
+        int endc = matrix[0].length - 1;
+        boolean flag = false;
+        while (tr < endr + 1) {
+            printLevel(matrix, tr, tc, dr, dc, flag);
+            tr = tc == endc ? tr + 1 : tr;
+            tc = tc == endc ? tc : tc + 1;
+            dc = dr == endr ? dc + 1 : dc;
+            dr = dr == endr ? dr : dr + 1;
+            flag = !flag;
         }
     }
 
-    public
+    public static void printLevel(int[][] matrix, int tr, int tc, int dr, int dc, boolean flag) {
+        if (flag) {
+            while (tr < dr + 1) {
+                System.out.print(matrix[tr++][tc--] + " ");
+            }
+        } else {
+            while (dr > tr - 1) {
+                System.out.print(matrix[dr--][dc++] + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+        printMatrixZigZag(matrix);
+
+    }
+
 }
---
+++ src/LeetCode/ArrayAndMatrix/Problem_05_MinLengthForSort.java
@@ -0,0 +1,7 @@
+package LeetCode.ArrayAndMatrix;
+
+/**
+ * 需要排序的最短子数组长度
+ */
+public class Problem_05_MinLengthForSort {
+}
