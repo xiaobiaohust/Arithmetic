2f2395d1ed3a5869045746835bae67655831703e
xiaobiao <343003079@qq.com>
2020-09-21 19:22:48 +0800

    9.21 4个,还有未完成

---
+++ src/LeetCode/ArrayAndMatrix/Problem_25_SmallestMissNum.java
@@ -0,0 +1,9 @@
+package LeetCode.ArrayAndMatrix;
+
+/**
+ * 数组中未出现的最小正整数
+ * 给定一个无序整形数组arr，找到数组中未出现的最小正整数
+ * 要求：时间复杂度O(N)，额外空间复杂度O(1)
+ */
+public class Problem_25_SmallestMissNum {
+}
---
+++ src/LeetCode/ArrayAndMatrix/Problem_26_MaxGap.java
@@ -0,0 +1,12 @@
+package LeetCode.ArrayAndMatrix;
+
+/**
+ * 数组排序之后相邻数的最大差值
+ * 给定一个整形数组arr，返回排序之后相邻两数的最大差值
+ * 要求：时间复杂度O(N)，额外空间复杂度O(N)
+ *
+ * 思路：直接使用排序算法，时间复杂度O(NlogN)，利用桶排序的思想可以实现
+ * 时间复杂度O(N)。额外空间复杂度O(N)
+ */
+public class Problem_26_MaxGap {
+}
