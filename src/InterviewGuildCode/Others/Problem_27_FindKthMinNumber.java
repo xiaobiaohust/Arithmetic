08fc3cf83934a069084de399e41f0c23df5729cd
xiaobiao <343003079@qq.com>
2020-10-10 18:19:34 +0800

    10.9 3个,

--- src/InterviewGuildCode/Others/Problem_27_FindKthMinNumber.java
+++ src/InterviewGuildCode/Others/Problem_27_FindKthMinNumber.java
@@ -6,7 +6,15 @@ package InterviewGuildCode.Others;
  * 思路1：
  *      求第K小的数，最开始的想法是最大堆，但是空间复杂度不满足要求
  *
- * 思路2：
+ * 思路2：利用之前的求两个长度相同排序数组的上中位数
+ *  假设长度较短的数组shortArr长S，长度较长的数组longArr长L
+ *  1：K<1 或者 K>S+L,
+ *  2：K<S，，则在长短两个数组的最前面分别取K个元素，求中位数
+ *  3：K>L，令m=L+S-K,则shortArr中[1,S-m-1],不可能是第K个元素,而且是之前的元素；
+ *  longArr中[1,L-m-1]也不可能是第K个元素，而且是之前的元素，
+ *  如果shortArr[S-m]大于longArr最后一个元素，则shortArr[S-m]是第K个元素
+ *  如果longArr[L-m]大于shortArr最后一个元素，则longArr[L-m]是第K个元素
+ *  以上都不是则在shortArr[]
  *
  */
 public class Problem_27_FindKthMinNumber {
