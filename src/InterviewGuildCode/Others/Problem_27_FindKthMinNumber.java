a20641d52cffd9fbdeed50e0be4d98704375545f
xiaobiao <343003079@qq.com>
2020-10-11 23:15:00 +0800

    9.17 晚上 3道题

--- src/InterviewGuildCode/Others/Problem_27_FindKthMinNumber.java
+++ src/InterviewGuildCode/Others/Problem_27_FindKthMinNumber.java
@@ -4,18 +4,25 @@ package InterviewGuildCode.Others;
  * 在两个排序数组中找到第K小的数
  * 要求：时间复杂度O(log(min(M,N)))，额外空间复杂度O(1)
  * 思路1：
- *      求第K小的数，最开始的想法是最大堆，但是空间复杂度不满足要求
- *
+ * 求第K小的数，最开始的想法是最大堆，但是空间复杂度不满足要求
+ * <p>
  * 思路2：利用之前的求两个长度相同排序数组的上中位数
- *  假设长度较短的数组shortArr长S，长度较长的数组longArr长L
- *  1：K<1 或者 K>S+L,
- *  2：K<S，，则在长短两个数组的最前面分别取K个元素，求中位数
- *  3：K>L，令m=L+S-K,则shortArr中[1,S-m-1],不可能是第K个元素,而且是之前的元素；
- *  longArr中[1,L-m-1]也不可能是第K个元素，而且是之前的元素，
- *  如果shortArr[S-m]大于longArr最后一个元素，则shortArr[S-m]是第K个元素
- *  如果longArr[L-m]大于shortArr最后一个元素，则longArr[L-m]是第K个元素
- *  以上都不是则在shortArr[]
- *
+ * 假设长度较短的数组shortArr长S，长度较长的数组longArr长L
+ * 1：K<1 或者 K>S+L,
+ * 2：K<S，，则在长短两个数组的最前面分别取K个元素，求中位数
+ * 3：K>L，令m=L+S-K,则shortArr中[1,S-m-1],不可能是第K个元素,而且是之前的元素；
+ * longArr中[1,L-m-1]也不可能是第K个元素，而且是之前的元素，
+ * 如果shortArr[S-m]大于longArr最后一个元素，则shortArr[S-m]是第K个元素
+ * 如果longArr[L-m]大于shortArr最后一个元素，则longArr[L-m]是第K个元素
+ * 以上都不是则在shortArr[S-m+1:S],longArr[L-m-1:L]求取中位数
+ * 4：S<k<L，对于shortArr都有可能是是第K个元素，对于longArr[k-S:K]可能是第K个元素
+ * 判断longArr[k-S]是否大于shortArr最后一个元素，大于则直接返回longArr[k-S]
+ * 小于，则在shortArr[1:S],longArr[k-s+1,k]求取中位数
  */
 public class Problem_27_FindKthMinNumber {
+    public static int findKthNum(int[] arr1, int[] arr2, int K) {
+        if (arr1 == null || arr2 == null || K < 1 || K > arr1.length + arr2.length) {
+            throw new RuntimeException("valid");
+        }
+    }
 }
