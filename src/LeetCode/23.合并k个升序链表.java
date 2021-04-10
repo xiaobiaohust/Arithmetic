b77b69a835201faff677082da928c9d5789a1aa1
xiaobiao <343003079@qq.com>
2021-04-10 15:38:23 +0800

    1

--- src/LeetCode/23.合并k个升序链表.java
+++ src/LeetCode/23.合并k个升序链表.java
@@ -17,6 +17,23 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null||lists.size()==0){
+            return null;
+        }
+        PriorityQueue<ListNode>queeu= new PriorityQueue<ListNode>(lists.size(),new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if(o1.val<o2.val){
+                    return -1;
+                }else if(o1.val==o2.val){
+                    return 0;
+                }else{
+                    return -1;
+                }
+            }
+        });
+        ListNode prehead = new ListNode(-1);
+        ListNode cur =prehead;
 
     }
 }
