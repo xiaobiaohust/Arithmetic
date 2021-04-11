61e1da8140b1d7cc04437addeac2c7d1be722b71
xiaobiao <343003079@qq.com>
2021-04-11 22:23:42 +0800

    测试

--- src/LeetCode/23.合并k个升序链表.java
+++ src/LeetCode/23.合并k个升序链表.java
@@ -17,10 +17,10 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null||lists.size()==0){
+        if(lists==null||lists.length==0){
             return null;
         }
-        PriorityQueue<ListNode>queeu= new PriorityQueue<ListNode>(lists.size(),new Comparator<ListNode>(){
+        PriorityQueue<ListNode>queue= new PriorityQueue<ListNode>(lists.length,new Comparator<ListNode>(){
             @Override
             public int compare(ListNode o1,ListNode o2){
                 if(o1.val<o2.val){
@@ -28,12 +28,26 @@ class Solution {
                 }else if(o1.val==o2.val){
                     return 0;
                 }else{
-                    return -1;
+                    return 1;
                 }
             }
         });
         ListNode prehead = new ListNode(-1);
         ListNode cur =prehead;
+        for(ListNode node :lists){
+            if(node!=null){
+                queue.add(node);
+            }
+        }
+        while(!queue.isEmpty()){
+            cur.next = queue.poll();
+            cur = cur.next;
+            if(cur.next!=null){
+                queue.add(cur.next);
+            }
+        }
+        return prehead.next;
+
 
     }
 }
