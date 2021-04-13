0864b47a7d5a75e2b0a3b6820c285d9c61ec3bee
xiaobiao <343003079@qq.com>
2021-04-13 12:03:19 +0800

    1

---
+++ src/LeetCode/143.重排链表.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=143 lang=java
+ *
+ * [143] 重排链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        // 使用快慢指针找到中间节点，从中间断开变为两个链表，后一个链表进行翻转，在将两个链表进行拼接
+
+    }
+}
+// @lc code=end
+
