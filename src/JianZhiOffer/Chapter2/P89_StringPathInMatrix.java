2993c719e66c2b13718985cfda066c70ac655005
xiaobiao <343003079@qq.com>
2021-07-22 20:54:41 +0800

    7.22 3道

---
+++ src/JianZhiOffer/Chapter2/P89_StringPathInMatrix.java
@@ -0,0 +1,9 @@
+package JianZhiOffer.Chapter2;
+
+/**
+ * 矩阵中的路径
+ * 判断句子中是否有某条路径所代表的的字符串等于给定字符串
+ * 参考 LeetCode -79
+ */
+public class P89_StringPathInMatrix {
+}
---
+++ src/JianZhiOffer/Chapter2/P92_RobotMove.java
@@ -0,0 +1,22 @@
+package JianZhiOffer.Chapter2;
+
+/**
+ * 机器人的运动范围
+ * 使用回溯法
+ */
+public class P92_RobotMove {
+    public static int movingCount(int threshold,int rowlen,int collen){
+        for(int i=0;i<rowlen;++i){
+            for(int j=0;j<collen;++j){
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+        System.out.println(movingCount(0,3,4)); //1
+        System.out.println(movingCount(1,3,4)); //3
+        System.out.println(movingCount(9,2,20)); //36
+    }
+}
