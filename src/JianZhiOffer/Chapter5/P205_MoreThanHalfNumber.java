4f3598ea9fc345c18568c1fbc9507bd0a495c368
xiaobiaohust <xiaobiao_my@163.com>
2021-08-14 23:33:30 +0800

    4.9 全部更新

--- src/JianZhiOffer/Chapter5/P205_MoreThanHalfNumber.java
+++ src/JianZhiOffer/Chapter5/P205_MoreThanHalfNumber.java
@@ -2,6 +2,7 @@ package JianZhiOffer.Chapter5;
 
 /**
  * 数组中出现次数超过一半的数字
+ * 参考 面试指南
  */
 public class P205_MoreThanHalfNumber {
 }
---
+++ src/JianZhiOffer/Chapter5/最小的k个数.java
@@ -0,0 +1,8 @@
+package JianZhiOffer.Chapter5;
+
+/**
+ * 思路1： partition思想，直到找到第k个数，左边的全部是最小的。时间复杂度O(N)，需要修改数组，无法处理海量数据
+ * 思路2：最大堆。时间复杂度O(NlogK)，不需要改数组，适合海量数据
+ */
+public class 最小的k个数 {
+}
